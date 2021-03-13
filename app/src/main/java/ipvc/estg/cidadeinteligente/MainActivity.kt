77f4269ae76bb54ec89ee47216e3a8def022810d
monicalima18666 <61920974+monicalima18666@users.noreply.github.com>
2021-03-13 18:10:09 +0000

    pagina login, pagina registo

--- app/src/main/java/ipvc/estg/cidadeinteligente/MainActivity.kt
+++ app/src/main/java/ipvc/estg/cidadeinteligente/MainActivity.kt
@@ -1,11 +1,18 @@
 package ipvc.estg.cidadeinteligente
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun button2(view: View) {
+        val intent = Intent(this, login::class.java)
+        startActivity(intent)
     }
 }
