800bd7837182520b983bf93be228edc50e7fb9a9
monicalima18666 <61920974+monicalima18666@users.noreply.github.com>
2021-03-14 19:37:16 +0000

    NotaDao

--- app/src/main/java/ipvc/estg/cidadeinteligente/dao/NotaDao.kt
+++ app/src/main/java/ipvc/estg/cidadeinteligente/dao/NotaDao.kt
@@ -10,10 +10,13 @@ import ipvc.estg.cidadeinteligente.entities.Nota
 interface NotaDao {
 
     @Query("SELECT * FROM nota_table ORDER BY titulo ASC")
-    fun getAlphabetizedWords(): LiveData<List<Nota>>
+    fun getAllNotas(): LiveData<List<Nota>>
 
-//    @Query("SELECT * FROM nota_table WHERE descricao == :name")
-//    fun getTitulosFromDescricao(name: String): LiveData<List<Nota>>
+   @Query("SELECT * FROM nota_table WHERE descricao == :descricao")
+    fun getTitulosFromDescricao(descricao: String): LiveData<List<Nota>>
+
+    @Query("SELECT * FROM nota_table WHERE titulo == :titulo")
+    fun getDescricaoFromTitulos(titulo: String): LiveData<List<Nota>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(nota: Nota)
@@ -25,7 +28,7 @@ interface NotaDao {
     suspend fun updateNota(nota: Nota)
 
     @Query("DELETE FROM nota_table where titulo == :titulo")
-    suspend fun deleteByCity(titulo: String)
+    suspend fun deleteByTitulo(titulo: String)
 
     @Query("UPDATE nota_table SET descricao=:descricao WHERE titulo == :titulo")
     suspend fun updateDescricaoFromTitulo(titulo: String, descricao: String)
--- app/src/main/java/ipvc/estg/cidadeinteligente/db/NotaDB.kt
+++ app/src/main/java/ipvc/estg/cidadeinteligente/db/NotaDB.kt
@@ -1,4 +1,75 @@
 package ipvc.estg.cidadeinteligente.db
 
-class NotaDB {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import ipvc.estg.cidadeinteligente.dao.NotaDao
+import ipvc.estg.cidadeinteligente.entities.Nota
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+
+@Database(entities = arrayOf(Nota::class), version = 8, exportSchema = false)
+
+public abstract class NotaDB : RoomDatabase() {
+
+    abstract fun notaDao(): NotaDao
+
+
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    var notaDao = database.notaDao()
+
+                    // Delete all content here.
+                    notaDao.deleteAll()
+
+                    // Add sample cities.
+                    var nota = Nota (1, "TRANSITO", "RUA 25 DE ABRIL")
+                    notaDao.insert(nota)
+                    nota = Nota(2, "Obras", "Perto de casa")
+                    notaDao.insert(nota)
+                    nota = Nota(3, "Tampa", "Desviada")
+                    notaDao.insert(nota)
+
+                }
+            }
+        }
+    }
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NotaDB? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): NotaDB {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NotaDB::class.java,
+                    "notas_database",
+                )
+                    //estratégia de destruição
+                    .fallbackToDestructiveMigration()
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
